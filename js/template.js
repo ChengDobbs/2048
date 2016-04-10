5416989e581875292a7e2c7986988d990a242622
ChengDobbs <dobbs.ch26@gmail.com>
2016-04-10 22:38:13 +0800

    更新滨江掠影

---
+++ js/template.js
@@ -0,0 +1,16 @@
+$(function(){//banner的三个滑动链接
+    if ($(".smooth-scroll").length>0) {
+			$('.smooth-scroll a[href*=#]:not([href=#]), a[href*=#]:not([href=#]).smooth-scroll').click(function() {
+				if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+					var target = $(this.hash);
+					target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+					if (target.length) {
+						$('html,body').animate({
+							scrollTop: target.offset().top-151
+						}, 1000);
+						return false;
+					}
+				}
+			});
+		}
+});
